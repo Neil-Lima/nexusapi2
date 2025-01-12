5d0a38a8e72d59e826c8c22e727588f0773ac7bd
Neil-Lima <110132286+Neil-Lima@users.noreply.github.com>
2025-01-11 23:47:46 -0300

    d

--- frontend/src/app/feed/page.jsx
+++ frontend/src/app/feed/page.jsx
@@ -2,6 +2,8 @@
 import React from 'react';
 import FeedMainComp from '@/components/feed/main/FeedMainComp';
 
+export const dynamic = 'force-dynamic'
+
 export default function FeedPage() {
   return (
     <div>
--- frontend/src/app/user/page.jsx
+++ frontend/src/app/user/page.jsx
@@ -5,6 +5,8 @@ import { GradientBackground } from '@/styles/GlobalStyles';
 import UserMainComp from '@/components/user/main/components/UserMainComp';
 import NavMenuComp from '@/shared/navbar/components/NavMenuComp';
 
+export const dynamic = 'force-dynamic'
+
 export default function UserPage() {
   const { theme } = useTheme();
 
